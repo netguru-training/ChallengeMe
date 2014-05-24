bbb228d265ebe0a807b6d129ca68ca9b998a8d75
Michal Kazmierczak <miniuka@interia.pl>
2014-05-24 13:57:53 +0200

    add dirty words filter

--- app/models/team.rb
+++ app/models/team.rb
@@ -8,4 +8,21 @@ class Team
  
   has_and_belongs_to_many :tournament
   has_and_belongs_to_many :users
+
+  DIRTY_WORDS = %W{
+      kurcze
+  }
+
+  field :name, type: String
+
+  has_many :membership
+
+  validates_presence_of :name
+  validate :filter_dirty_words
+
+  def filter_dirty_words
+    if DIRTY_WORDS.include?(name)
+      errors.add(:name, 'Team name contains dirty word!')
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+user1 = User.first_or_create(:email => "user1@example.com", :password => "changeme")
+user2 = User.first_or_create(:email => "user2@example.com", :password => "changeme")
+user3 = User.first_or_create(:email => "user3@example.com", :password => "changeme")
+user4 = User.first_or_create(:email => "user4@example.com", :password => "changeme")
+
+team1 = Team.first_or_create(:name => "Team A")
+team2 = Team.first_or_create(:name => "Team B")
+
+membership1 = Membership.first_or_create(:user => user1, :team => team1)
+membership2 = Membership.first_or_create(:user => user2, :team => team1)
+membership3 = Membership.first_or_create(:user => user3, :team => team2)
+membership4 = Membership.first_or_create(:user => user4, :team => team2)
+
+tournament1 = Tournament.first_or_create()
