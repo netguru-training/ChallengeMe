fcb0d03f92615e57d7c771cbb23f5c8970e46ced
misiaak <dominika.kucharska@gmail.com>
2014-05-24 16:29:45 +0200

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 ChallengeMe::Application.routes.draw do
   root 'tournaments#index'
-
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
-
-  resources :tournaments
   resources :teams
+  resources :tournaments 
+
 end
