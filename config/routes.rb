70172d75b90cab3caffddc7efe665078a76bf764
Mateusz Wilczyński <majko7@interia.pl>
2014-05-24 14:34:31 +0200

    teams

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,5 @@ ChallengeMe::Application.routes.draw do
   root 'tournaments#index'
 
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
-  #devise_scope :user do
-  #  get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
-  #end
+  resources :teams
 end
